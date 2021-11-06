0db68f666a1d78f35ddfab8ae94414defe71aa0e
Chokko2 <max.risku@gmail.com>
2021-11-06 10:17:09 +0200

    added better physics and fixed shooting bug

--- player.py
+++ player.py
@@ -9,6 +9,7 @@ class Player(pygame.sprite.Sprite):
 		self.image.fill((200, 0, 50))
 		self.rect = self.image.get_rect(topleft = pos) # get the rect
 		self.start_pos = pos # define the start position
+		self.direction_x = 0 # in which direction on the x axis we're facingt
 		# get the direction velocities
 		self.x_vel = 0
 		self.y_vel = 0
@@ -19,7 +20,7 @@ class Player(pygame.sprite.Sprite):
 		# check that  haven't double jumped yet and that we're ready to jump
 		if self.jumps < 1 and self.double_jump_cooldown < 0:
 			if self.y_vel != 0: self.jumps += 1 # check if that jump was out doublejump, if so make it so that we can't doublejump again
-			self.y_vel = -22 # make the y velocity negative so that we're going UP
+			self.y_vel = -35 # make the y velocity negative so that we're going UP
 			self.double_jump_cooldown = 10 # set the cooldown
 
 	def get_input(self): # handle the keyboard input
@@ -28,9 +29,11 @@ class Player(pygame.sprite.Sprite):
 		# increase or decrese the x velocity depending on what direction key is pressed
 		# ( gives a sliding illusion )
 		if keys[pygame.K_RIGHT] or keys[pygame.K_d]:
-			self.x_vel += 1.5
+			self.x_vel += 1
+			self.direction_x = 1
 		if keys[pygame.K_LEFT] or keys[pygame.K_a]:
-			self.x_vel -= 1.5
+			self.x_vel -= 1
+			self.direction_x = -1
 
 		# jump if we're pressing x or space
 		if keys[pygame.K_x] or keys[pygame.K_SPACE]:
@@ -38,7 +41,7 @@ class Player(pygame.sprite.Sprite):
 
 	def update_y(self):
 		self.double_jump_cooldown -= 1 # decrease the player's jump cooldown
-		self.y_vel += 1.5  # increase the y velocity (to make the player's fall quadratic)
+		self.y_vel += 3  # increase the y velocity (to make the player's fall quadratic)
 		self.rect.centery += self.y_vel # increase the player's y with the y velocity (makes the player fall)
 
 	def update(self):
